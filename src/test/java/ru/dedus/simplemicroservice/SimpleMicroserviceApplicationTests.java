e1065551e0c1b78454134a2bc33057ef12664673
Strabatras <strabatras@gmail.com>
2022-02-03 22:32:39 +0300

    Initial project

---
+++ src/test/java/ru/dedus/simplemicroservice/SimpleMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dedus.simplemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
